06bdd57dbc108f247913a4ff71c1a9f68b596933
pahrizal zal <phrizal077@gmail.com>
2021-02-24 12:21:46 +0700

    Update Projek

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/index', function () {
+    return "Ini index";
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
