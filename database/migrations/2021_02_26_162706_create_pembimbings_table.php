62e332f6fd1b6e5eb8940d277b59e7549406d254
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2021-02-26 23:35:30 +0700

    buat file migration

---
+++ database/migrations/2021_02_26_162706_create_pembimbings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembimbingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembimbings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembimbings');
+    }
+}
