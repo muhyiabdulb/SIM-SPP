56e9c134cdbf5823bb27dc14217fe0f9b6b5d8d4
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2021-02-27 01:34:20 +0700

    ngatur alur login role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Http\Request;
+    
 class LoginController extends Controller
 {
     /*
@@ -36,5 +37,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        if ($user->hasRole('kepsek')) {
+            return redirect()->route('kepsek.page');
+        } elseif ($user->hasRole('pegawai')) {
+            return redirect()->route('pegawai.page');
+        }
+        return redirect()->route('ortu.page');
     }
 }
