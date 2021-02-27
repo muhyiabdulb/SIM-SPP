74dd4bff25cb3d8f28d480cb9fb489a7065d9751
saddamazizirfana <saddamcosta14@gmail.com>
2021-02-27 15:13:58 +0700

    Memasukkan aseset tamplate klorofil

---
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -0,0 +1,14 @@
+<div id="sidebar-nav" class="sidebar">
+			<div class="sidebar-scroll">
+				<nav>
+					<ul class="nav">
+						<li><a href="/" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+						<li><a href="{{url('/datas')}}" class=""><i class="lnr lnr-user"></i> <span>Data Siswa</span></a></li>
+						<li><a href="{{url('/works')}}" class=""><i class="lnr lnr-pencil"></i> <span>Work</span></a></li>
+						<li><a href="{{url('/buktis')}}" class=""><i class="lnr lnr-book"></i> <span>Bukti</span></a></li>
+						<li><a href="{{url('/pemrays')}}" class=""><i class="lnr lnr-user"></i> <span>Pembimbing Rayon</span></a></li>
+						<li><a href="{{url('/rayons')}}" class=""><i class="lnr lnr-map-marker"></i> <span>Rayon</span></a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,51 @@
+
+<html lang="en">
+
+<head>
+	<title>Dashboard | Klorofil - Free Bootstrap Dashboard Template</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="{{asset('admin/assets/vendor/bootstrap/css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('admin/assets/vendor/font-awesome/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{asset('admin/assets/vendor/linearicons/style.css')}}">
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="{{asset('admin/assets/css/main.css')}}">
+	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+	<link rel="stylesheet" href="{{asset('admin/assets/css/demo.css')}}">
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="{{asset('admin/assets/img/apple-icon.png')}}">
+	<link rel="icon" type="image/png" sizes="96x96" href="{{asset('admin/assets/img/favicon.png')}}">
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		@include('layouts.includes._navbar')
+		<!-- LEFT SIDEBAR -->
+		@include('layouts.includes._sidebar')
+		<!-- END LEFT SIDEBAR -->
+		<!-- MAIN -->
+		@yield('content')
+		<!-- END MAIN -->
+		<div class="clearfix"></div>
+		<footer>
+			<div class="container-fluid">
+				<p class="copyright">Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+</p>
+			</div>
+		</footer>
+	</div>
+	<!-- END WRAPPER -->
+	<!-- Javascript -->
+	<script src="{{asset('admin/assets/vendor/jquery/jquery.min.js')}}"></script>
+	<script src="{{asset('admin/assets/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('admin/assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+	<script src="{{asset('admin/assets/scripts/klorofil-common.js')}}"></script>
+	
+</body>
+
+</html>
