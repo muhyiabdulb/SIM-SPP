d523782a2159179c35c08ef757887bc9ed494b24
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2021-03-05 00:17:04 +0700

    nambah nama

--- resources/views/layouts/includes/_navbar.blade.php
+++ resources/views/layouts/includes/_navbar.blade.php
@@ -79,7 +79,7 @@
             <li class="dropdown"><a href="#" data-toggle="dropdown"
                     class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                     <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-                    <div class="d-sm-none d-lg-inline-block">Hi,</div>
+                    <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
                     <div class="dropdown-title">Logged in 5 min ago</div>
