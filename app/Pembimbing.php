aa6f6aeb3e92bfb78a4bdf97d5d98b3d12447b15
muhyiabdulb <muhyiabdulbasith660@gmail.com>
2021-03-05 23:19:53 +0700

    bikin function

--- app/Pembimbing.php
+++ app/Pembimbing.php
@@ -8,4 +8,16 @@ class Pembimbing extends Model
 {
     // SEMUA FIELD DIMASUKKAN
     protected $guarded = [];
+
+    // RELASI KEPADA TABEL RAYON
+    public function rayon()
+    {
+        return $this->belongsTo(Rayon::class);
+    }
+
+    // membuat function Image
+    public function getTakeImageAttribute()
+    {
+        return "/storage/" . $this->photo;
+    }
 }
