6287b9bf1972c9707f21c5d6317d07018df2bfe2
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-06 22:32:40 +0700

    nambah siswa_id pada fillable

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-       'photo', 'name', 'email', 'username', 'password',
+       'siswa_id', 'photo', 'name', 'email', 'username', 'password',
     ];
 
     /**
@@ -38,4 +38,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // RELASI KEPADA TABEL SISWA
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class);
+    }
+
+    // membuat function Image
+    public function getTakeImageAttribute()
+    {
+        return "/storage/" . $this->photo;
+    }
 }
