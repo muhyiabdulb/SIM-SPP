23edfabc042e7ec4a5015bf0d26662c8bd872a54
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-09 22:38:22 +0700

    hapus rencana

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -341,21 +341,6 @@
                 </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-              <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                  <i class="far fa-newspaper"></i>
-                </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>Total Rencana Pembayaran</h4>
-                  </div>
-                  <div class="card-body">
-                    {{ App\RencanaPembayaran::count() }}
-                  </div>
-                </div>
-              </div>
-            </div>
                 @elserole('pembimbing')
             <h1>Dashboard</h1>
           </div>
