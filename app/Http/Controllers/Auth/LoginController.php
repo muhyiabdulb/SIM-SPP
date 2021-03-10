67ceef1d384dedb776db2ca66d367857174050b8
sheila-jvn <jvnsheila@gmail.com>
2021-03-10 21:06:20 +0700

    logout fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
     
 class LoginController extends Controller
 {
@@ -51,5 +52,28 @@ class LoginController extends Controller
             return redirect()->route('kepsek.dashboard');
         }
         return redirect()->route('pembimbing.dashboard');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+        
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/login'); 
     }
 }
