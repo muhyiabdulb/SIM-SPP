d0668ebbd61d882b2b558f152ce8c7329b154712
bayufazri <bayufazri17@gmail.com>
2021-03-10 16:36:38 +0700

    delete yajra app.php

--- config/app.php
+++ config/app.php
@@ -231,10 +231,6 @@ return [
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-<<<<<<< HEAD
-=======
-        'Datatables' => Yajra\Datatables\DatatablesServiceProvider::class
->>>>>>> 413ed0dd6bdd47336d557f606857abe40adeca66
     ],
 
 ];
