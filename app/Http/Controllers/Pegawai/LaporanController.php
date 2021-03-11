5e11686fe9fe75dbc875a8db78ef904525f869a2
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-12 06:18:57 +0700

    buat laporan controller

--- app/Http/Controllers/Pegawai/LaporanController.php
+++ app/Http/Controllers/Pegawai/LaporanController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Pegawai;
 
-use App\{Pembayaran};
+use App\{Pembayaran, Siswa, DetailPembayaran};
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,7 +10,17 @@ class LaporanController extends Controller
 {
     public function spp()
     {
-        $pembayarans = Pembayaran::latest()->get();
-        return view('pegawai.laporan.laporanSPP');
+        $siswas = Siswa::latest()->get();
+        return view('pegawai.laporan.laporanSPP', compact('siswas'));
+    }
+
+    public function detailSPP($id)
+    {
+
+        $bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+
+        $detail = Siswa::with('detailPembayaran')->find($id);
+        // return $detail;
+        return view('pegawai.laporan.detailSPP', compact('detail'));
     }
 }
