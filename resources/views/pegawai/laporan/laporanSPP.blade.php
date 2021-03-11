ed2260be51c1dfe1801fff0ebd1f14e20cee1731
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-11 09:21:17 +0700

    buat file

--- resources/views/pegawai/laporan/laporanSPP.blade.php
+++ resources/views/pegawai/laporan/laporanSPP.blade.php
@@ -1,91 +1,72 @@
-@extends('layouts.master', ['title' => 'Laporan SPP'])
+@extends('layouts.master', ['title' => 'History Pembayaran'])
+
+@section('style')
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+@endsection
 
 @section('content')
 
+<section class="section">
+    <div class="section-header">
+        <h1>History Pembayaran</h1>
+    </div>
+</section>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-12 col-sm-12">
-         <div class="card">
+        <div class="card">
             <div class="card-header">
-                <h4>Laporan</h4>
                 <div class="card-header-action">
-                    <a href={{ route('pegawai.dashboard') }} class="btn btn-danger"><i class="fa fa-arrow-left"></i> Back</a>
-                 </div>
+                    <a href="{{ route('pegawai.pembayaran.bayar') }}" class="btn btn-primary"
+                        style="border-radius: 5px"><i class="fa fa-plus"></i> Bayar</a>
+                </div>
             </div>
             <div class="card-body ">
-                <!-- /.content-header -->
-                <section class="content">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-12">
-                                <!-- Main content -->
-                                    <!-- info row -->
-                                    <div class="row invoice-info">
-                                        <div class="col-sm-12 invoice-col text-center">
-                                            <address>
-                                                <strong>BUKTI PEMBAYARAN SPP</strong><br>
-                                               Rp. 350.000,-
-                                                <p style="font-size: 18px;">(Tiga ratus lima puluh ribu rupiah)</p>
-                                            </address>
-                                        </div>
-                                    <!-- /.col -->
-                                    <!-- /.col -->
-                                    </div>
-                                    <!-- /.row -->
-            
-                                    <!-- Table row -->
-                                    <div class="row">
-                                        <div class="col-12 table-responsive">
-                                            <table class="table table-striped">
-                                                <thead>
-                                                    <tr>
-                                                        <th>BULAN</th>
-                                                        <th>TANGGAL BAYAR</th>
-                                                        <th>PENERIMA</th>
-                                                        <th>PARAF</th>
-                                                        <th>DIKETAHUI ORANG TUA/WALI</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    
-                                                    <tr>
-                                                    <td>Januari</td>
-                                                    <td>14/07/2020</td>
-                                                    <td>Lia M</td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    </tr>
-                                                 
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    <!-- /.col -->
-                                    </div>
-                                    <!-- /.row -->
-            
-                                    <!-- /.col -->
-                                    </div>
-                                    <!-- /.row -->
-            
-                                    <!-- this row will not appear when printing -->
-                                    <div class="row no-print" style="margin-top: 10px">
-                                        <div class="col-12">
-                                            <a href="" onclick="window.print()" class="btn btn-danger" style="margin-right: 10px;margin-left:12px"><i class="fas fa-print"></i> Print</a>
-                                            <a href="" class="btn btn-primary float-right" style="margin-right: 5px;margin-left:10px"><i class="fas fa-download"></i> Generate PDF</a>                
-                                            <a href="" class="btn btn-default float-right" style="background:green;color:white;"><i class="fas fa-download"></i> Generate Excel</a>                
-                                       
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /.invoice -->
-                            </div>
-                        </div>
-                        <!-- /.row -->
-                    </div>
-                    <!-- /.container-fluid -->
-                </section>
-                <!-- /.content -->
+                <div class="table-responsive">
+                    <table class="table table-striped" id="myTable">
+                        <thead>
+                            <tr>
+                                <th>NO</th>
+                                <th>USER</th>
+                                <th>SISWA</th>
+                                <th>TANGGAL PEMBAYARAN</th>
+                                <th>TOTAL NOMINAL</th>
+                                <th>ACTION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($history as $item)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $item->user['name'] }}</td>
+                                <td>{{ $item->siswa['nama_siswa'] }}</td>
+                                <td>{{ $item->date }}</td>
+                                <td>Rp {{ number_format($item->total_nominal) }}</td>
+                                <td>
+                                <form action="" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <a href="{{ route('pegawai.pembayaran.detail', $item->id) }}" class="btn btn-primary btn-action mr-1"><i class="fa fa-eye"></i></a>
+                                        <button type="submit" onclick="return confirm('Anda Yakin ?')" class="btn btn-danger btn-action"><i class="fa fa-trash"></i></button> 
+                                </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="7" class="text-center"><h3>Data Kosong</h3></td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endsection
+
+@section('script')
+    <script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>   
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        } );
+    </script> 
 @endsection
