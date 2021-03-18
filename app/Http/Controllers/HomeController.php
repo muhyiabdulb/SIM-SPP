469f5ff80d2cde5fe28d75a1f04d6ab4e553135c
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-18 07:25:39 +0700

    nambah nungguak

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\{Pembayaran, DetailPembayaran,Siswa};
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $totalNunggak = Pembayaran::whereHas('siswa', function($q){
+                            $q->where('rayon_id', Auth::user()->rayon_id);
+                        })->count();
+
+        $nominalNunggak = DetailPembayaran::whereHas('siswa', function($q){
+                            $q->where('rayon_id', Auth::user()->rayon_id);
+                        })->sum('sisa_pembayaran');
+
+        // return $nominalNunggak;
+        return view('home', compact('nominalNunggak', 'totalNunggak'));
     }
 }
