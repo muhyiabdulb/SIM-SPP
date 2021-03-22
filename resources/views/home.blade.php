b3c4e9a751b01069c021c24759dbe9cf47b105c0
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-22 21:42:05 +0700

    nambah info bayar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -77,6 +77,70 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-primary">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Pembayaran</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ App\Pembayaran::count('total_nominal') }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="far fa-newspaper"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Nominal Uang Masuk</h4>
+                            </div>
+                            <div class="card-body">
+                                <h4>Rp {{ number_format(App\Pembayaran::sum('total_nominal')) }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="far fa-file"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Tunggakan</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $totalNunggakP }} Orang
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-success">
+                            <i class="fas fa-circle"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Nominal Tunggakan</h4>
+                            </div>
+                            <div class="card-body">
+                                Rp {{ number_format($nominalNunggakP) }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         @elserole('pegawai')
         <div class="section-header">
