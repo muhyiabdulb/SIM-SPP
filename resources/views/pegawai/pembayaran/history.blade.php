0e8ae9de762d7c4210ec6719f51a5503583b5dfb
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-22 21:20:02 +0700

    hapus delete

--- resources/views/pegawai/pembayaran/history.blade.php
+++ resources/views/pegawai/pembayaran/history.blade.php
@@ -6,67 +6,70 @@
 
 @section('content')
 
-<section class="section">
-    <div class="section-header">
-        <h1>History Pembayaran</h1>
-    </div>
-</section>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-12 col-sm-12">
-        <div class="card">
-            <div class="card-header">
-                <div class="card-header-action">
-                    <a href="{{ route('pegawai.pembayaran.bayar') }}" class="btn btn-primary"
-                        style="border-radius: 5px"><i class="fa fa-plus"></i> Bayar</a>
+    <section class="section">
+        <div class="section-header">
+            <h1>History Pembayaran</h1>
+        </div>
+    </section>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-header-action">
+                        <a href="{{ route('pegawai.pembayaran.bayar') }}" class="btn btn-primary"
+                            style="border-radius: 5px"><i class="fa fa-plus"></i> Bayar</a>
+                    </div>
                 </div>
-            </div>
-            <div class="card-body ">
-                <div class="table-responsive">
-                    <table class="table table-striped" id="myTable">
-                        <thead>
-                            <tr>
-                                <th>NO</th>
-                                <th>USER</th>
-                                <th>SISWA</th>
-                                <th>TANGGAL PEMBAYARAN</th>
-                                <th>TOTAL NOMINAL</th>
-                                <th>ACTION</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($history as $item)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $item->user['name'] }}</td>
-                                <td>{{ $item->siswa['nama_siswa'] }}</td>
-                                <td>{{ $item->date }}</td>
-                                <td>Rp {{ number_format($item->total_nominal) }}</td>
-                                <td>
-                                <form action="" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <a href="{{ route('pegawai.pembayaran.detail', $item->id) }}" class="btn btn-primary btn-action mr-1"><i class="fa fa-eye"></i></a>
-                                        <button type="submit" onclick="return confirm('Anda Yakin ?')" class="btn btn-danger btn-action"><i class="fa fa-trash"></i></button> 
-                                </td>
-                            </tr>
-                            @empty
-                            <tr>
-                                <td colspan="7" class="text-center"><h3>Data Kosong</h3></td>
-                            </tr>
-                            @endforelse
-                        </tbody>
-                    </table>
+                <div class="card-body ">
+                    <div class="table-responsive">
+                        <table class="table table-striped" id="myTable">
+                            <thead>
+                                <tr>
+                                    <th>NO</th>
+                                    <th>USER</th>
+                                    <th>SISWA</th>
+                                    <th>TANGGAL PEMBAYARAN</th>
+                                    <th>TOTAL NOMINAL</th>
+                                    <th>ACTION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($history as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->user['name'] }}</td>
+                                        <td>{{ $item->siswa['nama_siswa'] }}</td>
+                                        <td>{{ $item->date }}</td>
+                                        <td>Rp {{ number_format($item->total_nominal) }}</td>
+                                        <td>
+                                            <form action="" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <a href="{{ route('pegawai.pembayaran.detail', $item->id) }}"
+                                                    class="btn btn-primary btn-action mr-1"><i class="fa fa-eye"></i></a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="7" class="text-center">
+                                            <h3>Data Kosong</h3>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
 
 @section('script')
-    <script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>   
+    <script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
     <script>
-        $(document).ready( function () {
+        $(document).ready(function() {
             $('#myTable').DataTable();
-        } );
-    </script> 
+        });
+
+    </script>
 @endsection
