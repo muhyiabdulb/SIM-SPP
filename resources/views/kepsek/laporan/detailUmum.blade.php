b2a3cd839b8752b9190463ff9e6839638f168c4f
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-03-22 21:03:07 +0700

    laporan kepsek

---
+++ resources/views/kepsek/laporan/detailUmum.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.master', ['title' => 'Laporan Detail SPP'])
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Laporan Bayaran Umum</h4>
+                    <div class="card-header-action">
+                        <a href={{ route('kepsek.laporan.umum') }} class="btn btn-danger"><i class="fa fa-arrow-left"></i>
+                            Back</a>
+                    </div>
+                </div>
+                <div class="card-body ">
+                    <!-- /.content-header -->
+                    <section class="content">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12">
+                                    <!-- Main content -->
+                                    <!-- info row -->
+                                    <div class="row invoice-info">
+                                        <div class="col-sm-12 invoice-col text-center">
+                                            <address>
+                                                <h3>BUKTI PEMBAYARAN UMUM</h3><br>
+                                            </address>
+                                        </div>
+                                        <!-- /.col -->
+                                        <!-- /.col -->
+                                    </div>
+                                    <!-- /.row -->
+                                    <h5>Nama Siswa : {{ $siswa->nama_siswa }}</h5>
+                                    <!-- Table row -->
+                                    <div class="row">
+                                        <div class="col-12 table-responsive">
+                                            <table class="table table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>JENIS PEMBAYARAN</th>
+                                                        <th>TANGGAL BAYAR</th>
+                                                        <th>NOMINAL</th>
+                                                        <th>BAYAR</th>
+                                                        <th>SISA PEMBAYARAN</th>
+                                                        <th>STATUS</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @forelse ($detail as $item)
+                                                        <tr>
+                                                            <th>{{ $item->jenisPembayaran->jenis_pembayaran }}</th>
+                                                            <td>{{ $item->tanggal_transfer }}</td>
+                                                            <td>Rp {{ number_format($item->nominal) }}</td>
+                                                            <td>Rp {{ number_format($item->bayar) }}</td>
+                                                            <td>Rp {{ number_format($item->sisa_pembayaran) }}</td>
+                                                            <td>
+                                                                @if ($item->status === 'Sudah DiVerifikasi')
+                                                                    <span
+                                                                        class="badge rounded-pill bg-primary text-white">{{ $item->status }}</span>
+                                                                @else
+                                                                    <span
+                                                                        class="badge rounded-pill bg-warning text-white">{{ $item->status }}</span>
+                                                                @endif
+                                                            </td>
+                                                        </tr>
+                                                    @empty
+                                                        <tr>
+                                                            <td colspan="6" class="text-center">
+                                                                <h3>{{ $siswa->nama_siswa }} Belum Bayar</h3>
+                                                            </td>
+                                                        </tr>
+                                                    @endforelse
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <!-- /.col -->
+                                    </div>
+                                    <!-- /.row -->
+
+                                    <!-- /.col -->
+                                </div>
+                                <!-- /.row -->
+
+                                <!-- this row will not appear when printing -->
+                                <div class="row no-print" style="margin-top: 10px">
+                                    <div class="col-12">
+                                        <a href="" onclick="window.print()" class="btn btn-danger"
+                                            style="margin-right: 10px;margin-left:12px"><i class="fas fa-print"></i>
+                                            Print</a>
+                                        <a href="" class="btn btn-primary float-right"
+                                            style="margin-right: 5px;margin-left:10px"><i class="fas fa-download"></i>
+                                            Generate PDF</a>
+                                        <a href="" class="btn btn-default float-right"
+                                            style="background:green;color:white;"><i class="fas fa-download"></i> Generate
+                                            Excel</a>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.invoice -->
+                        </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+    </div>
+    </div>
+    </div>
+@endsection
---
+++ resources/views/kepsek/laporan/laporanUmum.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.master', ['title' => 'Laporan Bayarn Umum'])
+
+@section('style')
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+@endsection
+
+@section('content')
+
+    <section class="section">
+        <div class="section-header">
+            <h1>Laporan Bayaran Umum</h1>
+        </div>
+    </section>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-12 col-sm-12">
+            <div class="card">
+                <div class="card-body ">
+                    <div class="table-responsive">
+                        <table class="table table-striped" id="myTable">
+                            <thead>
+                                <tr>
+                                    <th>NO</th>
+                                    <th>PHOTO</th>
+                                    <th>NIS</th>
+                                    <th>NAMA</th>
+                                    <th>JENIS KELAMIN</th>
+                                    <th>ROMBEL</th>
+                                    <th>RAYON</th>
+                                    <th>ACTION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($siswas as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>
+                                            @if ($item->photo)
+                                                <img style="height:100px; width:100px; object-fit:cover; object-position:center;"
+                                                    class="card-img-top" src="{{ $item->takeImage }}">
+                                            @else
+                                                {{ 'Tidak Ada Photo' }}
+                                            @endif
+                                        </td>
+                                        <td>{{ $item->nis }}</td>
+                                        <td>{{ $item->nama_siswa }}</td>
+                                        <td>{{ $item->jenis_kelamin }}</td>
+                                        <td>{{ $item->rombel->nama_rombel }}</td>
+                                        <td>{{ $item->rayon->nama_rayon }}</td>
+                                        <td>
+                                            <a href="{{ route('kepsek.laporan.detailUmum', $item->id) }}"
+                                                class="btn btn-primary btn-action mr-1"><i class="fa fa-eye"></i></a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="7" class="text-center">
+                                            <h3>Data Kosong</h3>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+
+@section('script')
+    <script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+
+    </script>
+@endsection
