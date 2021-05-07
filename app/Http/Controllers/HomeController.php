b664ff856d1b8e2ceec120614b96c5338a15f61a
Muhyi Abdul Basith <muhyiabdulbasith660@gmail.com>
2021-05-08 01:51:22 +0700

    dashboard ortu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,14 +27,14 @@ class HomeController extends Controller
     {
         // Untuk Pegawai (Semua Siswa)
         $totalNunggakP = Pembayaran::whereHas('detailPembayaran', function($q){
-                                        $q->where('status', 'Belum Lunas');
+                                        $q->where('status', 'Nunggak');
                                     })->count();
 
         $nominalNunggakP = DetailPembayaran::sum('sisa_pembayaran');
-
+                                    
         // Untuk Rayon (Hanya Rayon)
         $totalNunggakR = Pembayaran::whereHas('detailPembayaran', function($q){
-                                        $q->where('status', 'Belum Lunas');
+                                        $q->where('status', 'Nunggak');
                                     })->whereHas('siswa', function($q){
                                         $q->where('rayon_id', Auth::user()->rayon_id);
                                     })->count();
@@ -42,8 +42,28 @@ class HomeController extends Controller
         $nominalNunggakR = DetailPembayaran::whereHas('siswa', function($q){
                                         $q->where('rayon_id', Auth::user()->rayon_id);
                                     })->sum('sisa_pembayaran');
+        
+        // Untuk Ortu (Satu Siswa)
+        $totalBelumLunasO = Pembayaran::whereHas('detailPembayaran', function($q){
+                                        $q->where('status', 'Belum Lunas');
+                                    })->whereHas('siswa', function($q){
+                                        $q->where('id', Auth::user()->siswa_id);
+                                    })->count();
 
-        // return $nominalNunggak;
-        return view('home', compact('nominalNunggakR', 'totalNunggakR', 'nominalNunggakP', 'totalNunggakP', ));
+        $nominalBelumLunasO = DetailPembayaran::where('status', 'Belum Lunas')->whereHas('siswa', function($q){
+                                        $q->where('id', Auth::user()->siswa_id);
+                                    })->sum('sisa_pembayaran');
+
+        $totalNunggakO = Pembayaran::whereHas('detailPembayaran', function($q){
+                                        $q->where('status', 'Nunggak');
+                                    })->whereHas('siswa', function($q){
+                                        $q->where('id', Auth::user()->siswa_id);
+                                    })->count();
+
+        $nominalNunggakO = DetailPembayaran::where('status', 'Nunggak')->whereHas('siswa', function($q){
+                                        $q->where('id', Auth::user()->siswa_id);
+                                    })->sum('sisa_pembayaran');
+
+        return view('home', compact('nominalNunggakR', 'totalNunggakR', 'nominalNunggakP', 'totalNunggakP', 'nominalBelumLunasO', 'totalBelumLunasO', 'nominalNunggakO', 'totalNunggakO', ));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -166,7 +166,7 @@
                                 <h4>Total Pembayaran</h4>
                             </div>
                             <div class="card-body">
-                                {{ App\Pembayaran::count('total_nominal') }}
+                                {{ App\Pembayaran::count('total_nominal') }} Orang
                             </div>
                         </div>
                     </div>
@@ -234,62 +234,96 @@
         </div>
         <div class="section-content">
             <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
                         <div class="card-icon bg-primary">
                             <i class="far fa-user"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Total Admin</h4>
+                                <h4>Nama Siswa</h4>
                             </div>
                             <div class="card-body">
-                                10
+                                {{ auth()->user()->siswa->nama_siswa }}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
                         <div class="card-icon bg-danger">
                             <i class="far fa-newspaper"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>News</h4>
+                                <h4>Total Nominal Uang Masuk</h4>
                             </div>
                             <div class="card-body">
-                                42
+                                {{-- <h6>Rp {{ number_format(App\Pembayaran::sum('total_nominal')) }}</h6> --}}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
                         <div class="card-icon bg-warning">
                             <i class="far fa-file"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Reports</h4>
+                                <h4>Total Belum Lunas</h4>
                             </div>
                             <div class="card-body">
-                                1,201
+                                {{ $totalBelumLunasO }} x
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="card card-statistic-1">
                         <div class="card-icon bg-success">
                             <i class="fas fa-circle"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4>Online Users</h4>
+                                <h4>Total Nominal Belum Lunas</h4>
                             </div>
                             <div class="card-body">
-                                47
+                                Rp {{ number_format($nominalBelumLunasO) }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="far fa-file"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Tunggakan</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ $totalNunggakO }} x
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-success">
+                            <i class="fas fa-circle"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Nominal Tunggakan</h4>
+                            </div>
+                            <div class="card-body">
+                                Rp {{ number_format($nominalNunggakO) }}
                             </div>
                         </div>
                     </div>
